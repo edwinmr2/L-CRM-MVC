25e546c1e592782291403cccb587a3c141de7225
EOHL <EOHL@DESKTOP-QGFE3E6>
2018-05-16 11:36:32 +0200

    Add project files.

---
+++ L-CRM-MVC/Controllers/LCRMHelper.cs
@@ -0,0 +1,39 @@
+﻿using L_CRM_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace L_CRM_MVC.Controllers
+{
+    public static class LCRMHelper
+    {
+
+        public static bool isValidPolicyHolder(Customer customer)
+        {
+            bool isValid = false;
+
+            if (string.IsNullOrEmpty(customer.Name) || string.IsNullOrEmpty(customer.PhoneNumber) || string.IsNullOrEmpty(customer.Address) ||
+                string.IsNullOrEmpty(customer.CustomerType.Description) || (customer.BillAmount < 0))
+                isValid = false; //if any of the fields are null or empty the 
+            else
+                isValid = true; //Else the values are valid
+
+
+            return isValid;
+        }
+
+        public static bool isValidLead(Customer customer)
+        {
+            bool isValid = false;
+
+            if (string.IsNullOrEmpty(customer.Name) || string.IsNullOrEmpty(customer.PhoneNumber))
+                isValid = false; //if any of the fields are null or empty the 
+            else
+                isValid = true; //Else the values are valid
+
+
+            return isValid;
+        }
+    }
+}
---
+++ L-CRM-MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(L_CRM_MVC.Startup))]
+namespace L_CRM_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
